6ef8901911fc4996f7a63f7b2e51e4870ca8e9e2
Rachit Kumar <Rachitkumar3710@gmail.com>
2025-09-19 22:12:13 +0530

    Add files via upload

---
+++ vote.java
@@ -0,0 +1,12 @@
+public class vote {
+    public static void main(String args[]){
+        int age = 19;
+        if(age>=18){
+            System.out.println("adult : drive, vote");
+            
+        }
+        else{
+             System.out.println("not adult");
+        }
+    }
+}
---
+++ whilepro.java
@@ -0,0 +1,13 @@
+// While loop program---//
+
+public class whilepro {
+    public static void main(String args[]){
+        int counter = 0;
+        while(counter<100){
+            System.out.println("Hello World!");
+            counter++;
+        }
+        System.out.println("Printed HW 100 times");
+    }
+    
+}
